4eb209c6f1ab05f40e12968300c80f0217dd7cb9
DzarZaki <dzarzaki2@gmail.com>
2023-05-25 10:40:32 +0700

    "Mendefinisikan fungsi-fungsi publik dari kelas DoubleLinkedList, termasuk konstruktor, penambahan node, pencarian, penghapusan, pengecekan kekosongan daftar, pengurutan secara menaik, pengurutan secara menurun, penghapusan seluruh daftar, dan pencarian data."

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -14,3 +14,13 @@ class DoubleLinkedList {
 private:
 	Node* START;
 public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool ddeleteNode(int rollNo);
+	bool listEmpty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchData();
+};
